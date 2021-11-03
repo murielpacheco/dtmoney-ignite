f841d735dad2843817f5c7ba607f5c459e764113
Muriel Damazio <murieldps@gmail.com>
2021-11-03 19:54:07 -0300

    Feat: Creating modal with react-modal. Service/api folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+import Modal from 'react-modal'
+
 import { createServer } from 'miragejs';
+import { useState } from "react";
+
 
 createServer({
   routes() {
@@ -24,10 +28,25 @@ createServer({
 })
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <Modal 
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle/>
     </>
   );

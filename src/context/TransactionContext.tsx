c748be80584534ddd389587f7f270c85af05d8bb
Muriel Damazio <murieldps@gmail.com>
2021-11-04 11:03:16 -0300

    Feat: Calcs at  Summary.tsx

--- src/context/TransactionContext.tsx
+++ src/context/TransactionContext.tsx
@@ -16,11 +16,13 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 
 type TransactionInput = Omit<Transaction , 'id' | 'createdAt'>
+
+
 
 export const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
@@ -38,8 +40,14 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
     .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) { 
-    api.post('/transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) { 
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    })
+    const {transaction} = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
 
